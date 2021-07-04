a1c29f8126e36c0bf9142d397b7ae4187a4fce78
Isabela Moraes <isabelagmoraes@hotmail.com>
2021-07-03 21:58:13 -0300

    Upload to git

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">
+  My skills
+</h1>
+
+<p align="center">
+  <a href="#features">Features</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#getting-started">Getting Started</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#reference">Reference</a>
+</p>
+
+## Features
+
+This project was developed with the following technologies:
+
+-  **[React Native](https://reactnative.dev/)** - A framework for creating native apps for Android and iOS with JavaScript and React.
+
+## Getting Started
+
+To clone and run this application, place from your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/isabelamoraes/ignite-react-native-classes-myskills.git ignite-react-native-classes-myskills
+
+# Go into the repository
+$ cd ignite-react-native-classes-myskills
+
+# Install dependencies
+$ yarn
+
+# Run the mobile application (Android)
+$ yarn android
+
+# Run the test
+$ yarn detox test -c android.emu.debug
+
+```
+
+## Reference
+
+This application was developed during the classes of the Chapter I of the React Native track on Ignite, training promoted by Rocketseat, and reused in the Chapter V to explore the tests concept on React Native.
---
+++ src/pages/Home.tsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TextInput,
+    Platform,
+    FlatList
+} from 'react-native';
+
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
+
+interface SkillData{
+    id: string;
+    name: string;
+}
+
+export function Home() {
+    const [newSkill, setNewSkill] = useState('');
+    const [mySkills, setMySkills] = useState<SkillData[]>([]);
+    const [gretting, setGretting] = useState('');
+
+    function handleAddNewSkill() {
+        const data = {
+            id: String(new Date().getTime()),
+            name: newSkill
+        }
+        setMySkills(oldState => [...oldState, data]);
+    }
+
+    function handleRemoveSkill(id: string){
+        setMySkills(oldState => oldState.filter(
+            skill => skill.id !== id
+        ));
+    }
+
+    useEffect(() => {
+        const currentHour = new Date().getHours();
+
+        if(currentHour < 12){
+            setGretting('Good morning');
+        }else if(currentHour >= 12 && currentHour < 18){
+            setGretting('Good afternoon');
+        }else{
+            setGretting('Good night')
+        }
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title} testID="welcome">
+                Welcome, Isabela
+            </Text>
+
+            <Text style={styles.gretting}>
+                {gretting}
+            </Text>
+
+            <TextInput
+                testID="input-new"
+                style={styles.input}
+                placeholder="New skill"
+                placeholderTextColor="#555"
+                onChangeText={setNewSkill}
+            />
+
+            <Button 
+                testID="button-add"
+                title="Add"
+                onPress={handleAddNewSkill}
+            />
+
+            <Text style={[styles.title, { marginVertical: 50 }]}>
+                My Skills
+            </Text>
+
+            {
+                mySkills &&
+                <FlatList
+                    testID="flatlist-skills"
+                    data={mySkills}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) => (
+                        <SkillCard 
+                            skill={item.name}
+                            onPress={() => handleRemoveSkill(item.id)}
+                        />
+                    )}
+                />
+            }            
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingVertical: 70,
+        paddingHorizontal: 30
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        padding: Platform.OS == 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7
+    },
+    gretting: {
+        color: '#FFF'
+    }
+})
